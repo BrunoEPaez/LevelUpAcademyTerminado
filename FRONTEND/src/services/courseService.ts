4981e622ffb47e1af4be58d406af64e17bab2033
BrunoEPaez <emmanuelnovo007@gmail.com>
2026-02-09 11:33:03 -0300

    Fix

--- FRONTEND/src/services/courseService.ts
+++ FRONTEND/src/services/courseService.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:3000'; // La dirección de tu servidor
+const API_URL = 'https://levelupacademyterminado.onrender.com'; // La dirección de tu servidor
 
 export const getCourses = async () => {
   const response = await axios.get(`${API_URL}/courses`);
