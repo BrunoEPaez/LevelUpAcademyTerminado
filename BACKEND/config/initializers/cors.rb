f6768ac5b35163961a5bdb4d43a8b01d02e65979
BrunoEPaez <emmanuelnovo007@gmail.com>
2026-02-08 23:23:53 -0300

    Fix

--- BACKEND/config/initializers/cors.rb
+++ BACKEND/config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:5173" # El puerto donde corre tu React
+    origins "https://a04651b3.levelupacademyterminado.pages.dev" # El puerto donde corre tu React
     resource "*",
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
